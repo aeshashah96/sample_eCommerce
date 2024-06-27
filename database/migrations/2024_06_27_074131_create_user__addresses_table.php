ba84cd36ad2329561369bf3b01e8f7f36226e7da
NikunjViramgami <nikunjviramgami2309@gmail.com>
2024-06-27 14:55:01 +0530

    #43 >> Create Database Schema

---
+++ database/migrations/2024_06_27_074131_create_user__addresses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user__addresses', function (Blueprint $table) {
+            $table->id();
+            $table->text('billing_address');
+            $table->text('shipping_address');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user__addresses');
+    }
+};
