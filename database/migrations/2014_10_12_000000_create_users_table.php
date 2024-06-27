4f3936db844d4c82ccdc264559289e55d93e8e08
NehalSolanki28 <nehalsolanki69@gmail.com>
2024-06-27 13:16:55 +0530

    #38:Create Schema for User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name',50);
+            $table->string('last_name',50);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('phoneNumber');
+            $table->string('user_logo')->default(null);
+            $table->boolean('isActive')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
