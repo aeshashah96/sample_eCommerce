1dab0dabfa53e1db05b9182a7dd462caea464297
NehalSolanki28 <nehalsolanki69@gmail.com>
2024-06-27 15:25:31 +0530

    #38:Create Schema for User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name',50);
             $table->string('last_name',50);
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->unsignedBigInteger('phoneNumber');
