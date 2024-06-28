c2dc12e07272d6df4873b0cc391fbbe372f34d12
Harshvardhan <harshvardhan01000@gmail.com>
2024-06-28 18:01:15 +0530

    banner crud is completed

--- app/Models/Banners.php
+++ app/Models/Banners.php
@@ -8,5 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Banners extends Model
 {
     use HasFactory;
-    protected $fillable = ['image','description']; 
+    protected $fillable = [
+        'image',
+        'description',
+        'banner_title',
+        'banner_url',
+        'sub_category_id'
+    ]; 
+    public function getSubCategory()
+    {
+        return $this->belongsTo(SubCategories::class,'sub_category_id','id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\BannerCrudController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -62,6 +63,7 @@ Route::middleware(['auth:adminApi'])->group(function(){
     Route::get('/adminprofile',[AdminAuthController::class,'fetch_admin_data']);
     Route::post('/editadminprofile',[AdminAuthController::class,'edit_admin_data']);
     Route::post('/changeadminpassword',[AdminAuthController::class,'change_admin_password']);
+    Route::resource('/banner',BannerCrudController::class);
 });
 
 
