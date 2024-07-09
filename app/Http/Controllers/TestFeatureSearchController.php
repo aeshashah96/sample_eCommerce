d8105206cfe666d4d75335a647c593e33a5b5509
Harshvardhan <harshvardhan01000@gmail.com>
2024-07-09 18:50:17 +0530

    [Backend Front - API Development ] searching filter completed

--- app/Http/Controllers/TestFeatureSearchController.php
+++ app/Http/Controllers/TestFeatureSearchController.php
@@ -35,34 +35,47 @@ class TestFeatureSearchController extends Controller
         return $data;
     }
     public function redirect_search($data,$id,$flag){
-       
+            //searching string in  category name
             $category = Categories::where('name','like',"%$id%");
             if($category->count()){
+                //redirect to category_search
                 $data = TestFeatureSearchController::category_search($data,$id);
                 $flag = true;
                 return ['flag'=>$flag,'data'=>$data];
             }
+
+            //searching string in sub-category name
             $sub_category = SubCategories::where('name','like',"%$id%");
             if($sub_category->count()){
+                //redirect to sub_category_search
                 $data = TestFeatureSearchController::sub_category_search($data,$id);
                 $flag = true;
                 return ['flag'=>$flag,'data'=>$data];
 
             }
+
+            //searching string in productColor table
             $color = ProductColor::where('color','like',"%$id%");
             if($color->count()){
+                //redirect to color_search
                 $data = TestFeatureSearchController::color_search($data,$id);
                 $flag = true;
                 return ['flag'=>$flag,'data'=>$data];
             }
+
+            //searching string in productSize table
             $size = ProductSize::where('size','like',"%$id%");
             if($size->count()){
+                //redirect to size_search
                 $data = TestFeatureSearchController::size_search($data,$id);
                 $flag = true;
                 return ['flag'=>$flag,'data'=>$data];
             }
+
+            //searching string in products table
             $product = Product::where('name','like',"%$id%");
             if($product->count()){
+                //redirect to product_search
                 $data = TestFeatureSearchController::product_search($data,$id);
                 $flag = true;
                 return ['flag'=>$flag,'data'=>$data];
@@ -71,7 +84,9 @@ class TestFeatureSearchController extends Controller
     }
 
     public function test_search($id){
-
+        /*
+        join category,subcategory,productColor,productSize tables to product table 
+        */
         $data = DB::table('products')
                 ->join('product_varients','products.id','=','product_varients.product_id')
                 ->join('categories','categories.id','=','products.category_id')
@@ -79,26 +94,47 @@ class TestFeatureSearchController extends Controller
                 ->leftJoin('product_sizes','product_sizes.id','=','product_varients.product_size_id',)
                 ->leftJoin('product_colors','product_colors.id','=','product_varients.product_color_id',);
                 
-        
+        /*
+        sending $data collecting and searched string to redirect_search function 
+        */
         $value =  TestFeatureSearchController::redirect_search($data,$id,$flag=false);
+        
+        /*
+        if records are found for whole string then $value['flag'] returns true and return data to serach or filter function
+        */
         if($value['flag']){
             return $value['data'];
             
         }
+        /*
+        if record not found for whole string then devide string by space and find for each word 
+        */
         $key_words = explode(" ",$id);
 
         foreach($key_words as $word){
+            /*
+            searching for each word of string using redirect_search function
+            */
             $value =  TestFeatureSearchController::redirect_search($data,$word,$flag=false);
             $data = $value['data'];
         }   
+        /*
+        return to search or filter function 
+         */
         return $value['data'];
         
     }
 
     public function search_filter(Request $request){
+        /*
+        sending searching string from param to test_search function
+        */
         $value = TestFeatureSearchController::test_search($request->search);
-        $data = $value->get('products.id')->toArray();
-        $final = array_unique(array_column($data, 'id'));
+        $data = $value->get('products.id')->toArray();      
+        /*
+        get search result products ids and getting data using model
+        */    
+        $final = array_unique(array_column($data, 'id'));   
         $product = Product::whereIn('id',$final)->with('productImages')->paginate(10);
         return response()->json([
             'success'=>true,
@@ -108,19 +144,33 @@ class TestFeatureSearchController extends Controller
         ]);
     }
     public function filter_feature(Request $request){
-        
+        /*
+        sending searching string from param to test_search function
+        */
         $value = TestFeatureSearchController::test_search($request->search);
+        /*
+        filter search results by price 
+        */
         if($request->price){
             $value->where('products.price','<',$request->price);
         }
+        /*
+        filter search results by color 
+        */
         if($request->color){
             $value->where('product_colors.color',$request->color);
         }
+        /*
+        filter search results by size 
+        */
         if($request->size){
             $value->where('product_sizes.size',$request->size);
         }
         $data = $value->get('products.id')->toArray();
         $final = array_unique(array_column($data, 'id'));
+        /*
+        get search result products ids and getting data using model
+        */    
         $product = Product::whereIn('id',$final)->with('productImages')->paginate(10);
         return response()->json([
             'success'=>true,
