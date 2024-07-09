451b5b41c79c9e1577ea807609026352557bf9cd
Sunil <soranisunil22@gmail.com>
2024-07-09 09:47:11 +0530

    #80:Order Crud

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -11,7 +11,7 @@ class OrdersController extends Controller
     //
 
     public function index(){
-        $order=Orders::paginate(10);
+        $order=Orders::with('user')->paginate(10);
         if($order){
             return response()->json(['success'=>true,'status'=>200,'message'=>'Order Get Successfully','data'=>$order]);
         }else{
@@ -37,7 +37,7 @@ class OrdersController extends Controller
         if($order){
             return response()->json(['success'=>true,'status'=>200,'message'=>'Order Get Successfully','data'=>$order]);
         }else{
-            return response()->json(['success'=>false,'status'=>404,'message'=>'No Order found']);
+            return response()->json(['success'=>false,'status'=>404,'message'=>'Order Not Found']);
         }
     }
 }
