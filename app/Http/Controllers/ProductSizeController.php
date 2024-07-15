218267e16eb34e2362594be22407a20ddf330b59
NehalSolanki28 <nehalsolanki69@gmail.com>
2024-07-15 09:33:17 +0530

    Bug Solved

--- app/Http/Controllers/ProductSizeController.php
+++ app/Http/Controllers/ProductSizeController.php
@@ -14,7 +14,7 @@ class ProductSizeController extends Controller
     public function index()
     {
         //get all sizes
-        $size=ProductSize::orderBy('created_at','DESC')->paginate(30);
+        $size=ProductSize::orderBy('created_at','DESC')->paginate(10);
         if($size){
 
             return response()->json(['success'=>true,'status'=>200,'message'=>'Product Size Get Successfully','data'=>$size]);
